5f9dbe2b13dabf83192209d6d4052611b9b4bcc8
Tyler <jon87tyler@gmail.com>
2019-03-29 16:24:16 -0700

    built out pages

--- src/routing/Router.js
+++ src/routing/Router.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { hot } from 'react-hot-loader/root'
 
-import { Home, About } from '../pages'
+import { Home, About, Portfolio, Contact } from '../pages'
 
 
 class Router extends React.Component {
@@ -13,6 +13,8 @@ class Router extends React.Component {
         <Switch>
           <Route path='/' component={ Home } exact />
           <Route path='/about' component={ About } exact />
+          <Route path='/portfolio' component={ Portfolio } exact />
+          <Route path='/contact' component={ Contact } exact />
         </Switch>
       </BrowserRouter>
     )
